85665fda485e24c60c739dd6eb9bdfcec291efea
latisyamaudina <21221752121@smkassalaambandung.sch.id>
2023-08-03 08:30:56 +0700

    latihan router

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,5 +1,6 @@
 <template>
     <h1>Detail {{ detail.nama }}</h1>
+    <img :src="getImgSrc(detail.img)" alt="Category Image" style="width : 300px"/>
     <h4>Harga: {{ detail.harga }}</h4>
     <h4>Keterangan : {{ detail.Keterangan }}</h4>
   </template>
@@ -13,11 +14,15 @@ export default {
     ],
     setup(props)
     {
+        const getImgSrc = (imgFileName) => {
+         return '../src/assets/img/' + imgFileName + '';
+       }
         const detail = produk["produk"].find(function(item) {
             return item.id == props.id_produk;
         });
         return{
-            detail
+            detail,
+            getImgSrc
         }
     }
 }
